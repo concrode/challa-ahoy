f52eb8d566e57feaa7d03a72cab8964d5e36d0ef
concrode <captainshenhua@gmail.com>
2021-01-23 22:31:51 +1100

    updates

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username:string;
+  password:string;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  loginUser () {
+    const loginUserData = {
+      username:this.username,
+      password:this.password
+    }
+
+    console.log(`${loginUserData.username} + ${loginUserData.password} `);
   }
 
 }
