c75d9400fcb70b0aef5a47657d500cc87c990606
concrode <captainshenhua@gmail.com>
2021-01-23 17:00:28 +1100

    updates

--- src/app/shared/visitor.service.ts
+++ src/app/shared/visitor.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Visitor } from './visitor.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
+@Injectable()
 export class VisitorService {
 
+  private visitor = new BehaviorSubject(Visitor);
+  currentVisitor= this.visitor.asObservable();
+
   constructor() { }
+
+  changeVisitor(visitor: Visitor) {
+    this.visitor.next(visitor);
+  }
 }
